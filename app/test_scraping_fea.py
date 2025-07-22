4f9ac4f6fe0819a8f9ba13dc5c92597454e1c978
Igor0Pires <igorpife@usp.br>
2025-07-22 19:27:11 -0300

    test: API_KEY imp.

--- app/test_scraping_fea.py
+++ app/test_scraping_fea.py
@@ -18,7 +18,7 @@ api_key_header = APIKeyHeader(name="X-API-Key", auto_error=True)
 async def get_api_key(api_key: str = Depends(api_key_header)):
     if api_key == API_KEY:
         return api_key
-    raise HTTPException(status_code=status.HTTP)
+    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Chave Inválida")
 
 class FormData(BaseModel):
     name: str
